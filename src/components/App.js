e1f9e5e993273ca42ef7ffc5c2e9a7b3688cb99a
Valeriia <valeriia.air@gmail.com>
2022-08-17 23:46:34 +0300

    App

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import { Searchbar } from './Searchbar/Searchbar'
 import { ImageGallery } from './ImageGallery/ImageGallery'
 
-export const App = () => {
-  return (
-    <div>
-      <Searchbar></Searchbar>
-      <ImageGallery></ImageGallery>
-    </div>
-  )
+const BASE_URL = 'https://pixabay.com/api/'
+const API_KEY = '19320063-cda7f2d635216fb573107b42d'
+
+export class App extends Component {
+  state = {
+    images: []
+  }
+  componentDidMount () {
+    fetch(
+      `https://pixabay.com/api/?key=19320063-cda7f2d635216fb573107b42d&q=yellow+flowers&image_type=photo`
+    )
+      .then(response => response.json())
+      .then(console.log)
+  }
+
+  render () {
+    return (
+      <div>
+        <Searchbar></Searchbar>
+        <ImageGallery></ImageGallery>
+      </div>
+    )
+  }
 }
