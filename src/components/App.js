5bcd8c68a01a44c0f6d026e3eed6d520acbf71c3
Valeriia <valeriia.air@gmail.com>
2022-08-18 23:52:36 +0300

    render img

--- src/components/App.js
+++ src/components/App.js
@@ -8,21 +8,29 @@ const API_KEY = '19320063-cda7f2d635216fb573107b42d'
 
 export class App extends Component {
   state = {
-    images: []
+    images: [],
+    loading: false
   }
+  
   componentDidMount () {
+    this.setState({loading: true})
     fetch(
       `https://pixabay.com/api/?key=19320063-cda7f2d635216fb573107b42d&q=yellow+flowers&image_type=photo`
     )
       .then(response => response.json())
-      .then(console.log)
+      .then(images => this.setState({ images: images.hits }))
+      .finally(() => this.setState({loading: false}))
   }
 
   render () {
     return (
       <div>
+        <div>
+          {this.state.loading && <h2>Loading...</h2>}
+          {this.state.images && <div>{this.state.images.likes}</div>}
+          </div>
         <Searchbar></Searchbar>
-        <ImageGallery></ImageGallery>
+        <ImageGallery images={this.state.images} />
       </div>
     )
   }
--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import { ImageGalleryItem } from '../ImageGalleryItem/ImageGalleryItem'
 
-export const ImageGallery = () => {
+export const ImageGallery = (images) => {
   return (
     <>
-      <ul class='gallery'>{/* <!-- Набір <li> із зображеннями --> */}</ul>
+      <ul class='gallery'></ul>
     </>
   )
 }
